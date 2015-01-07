6c8c1ad93cfaed568f082870f9412a78ba29539c
highya <827931245@qq.com>
2015-01-07 11:31:47 +0800

    xuwei solution

---
+++ Week1/remove-nth-node-from-end-of-list/xuwei.py
@@ -0,0 +1,14 @@
+class Solution:
+	def removeNthFromEnd(self,head,n):
+		if head is None or n == 0:
+			return None
+		p1,p2,m = listNode(0),head,n-1
+		p1.next = head
+		Head = p1
+		while m > 0:
+			p2 = p2.next
+			m -= 1
+		while p2.next:
+			p1,p2=p1.next,p2.next
+		p1.next = p1.next.next
+		return Head.next
