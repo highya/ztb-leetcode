bd98732c4ba178aa917c857705ef00ec191de29a
highya <xwiscas@163.com>
2015-01-07 18:33:22 +0800

    Update xuwei.py

--- Week3/Reverse-Linked-List-II/xuwei.py
+++ Week3/Reverse-Linked-List-II/xuwei.py
@@ -1,1 +1,15 @@
-s
+class Solution:
+    def reverseBetween(self, head, m, n):
+        if m == n:
+            return head
+        prev = ListNode(0)
+        prev.next = head
+        Head,p,num = prev,head,m
+        while num > 1:
+            p,prev,num = p.next,prev.next,num-1
+        for i in range(n-m):
+            q = p.next
+            p.next = q.next
+            q.next = prev.next
+            prev.next = q
+        return Head.next
