a65f13b8e04d009f5ee1ada9072e311d64a616b2
highya <xwiscas@163.com>
2015-01-07 16:09:48 +0800

    Update xuwei.py

--- Week2/Two-Sum/xuwei.py
+++ Week2/Two-Sum/xuwei.py
@@ -1,1 +1,9 @@
-class
+class Solution:
+    # @return a tuple, (index1, index2)
+    def twoSum(self, num, target):
+        mmap={}
+        for i in range(len(num)):
+            if num[i] not in mmap:
+                mmap[target-num[i]]=i
+            else:
+                return [mmap[num[i]]+1,i+1]
