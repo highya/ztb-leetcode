c3f9909827b2d792b722409052e47425d809ddde
highya <827931245@qq.com>
2015-01-07 10:17:13 +0800

    xuwei solution

---
+++ Week1/merge-two-sorted-lists/xuwei.py
@@ -0,0 +1,32 @@
+class Solution:
+	def mergeTwoLists(self, l1, l2):
+		nHead = ListNode(0)
+		lt, rt, backHead = l1, l2, nHead
+		while lt or rt:
+			if lt is None:
+				nHead.next, rt = rt, rt.next
+			elif rt is None:
+				nHead.next, lt = lt, lt.next
+			elif lt.val < rt.val:
+				nHead.next, lt = lt, lt.next
+			else
+			nHead.next, rt = rt, rt.next
+			nHead = nHead.next
+		return backHead.next
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
