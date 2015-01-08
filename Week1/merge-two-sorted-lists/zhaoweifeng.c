1cc27d5229e44c010a6b911f8d7ab14d8a7ab36a
wox88 <wox88@qq.com>
2015-01-08 12:57:52 +0800

    Update zhaoweifeng.c
    使用额外的结点作为头结点

--- Week1/merge-two-sorted-lists/zhaoweifeng.c
+++ Week1/merge-two-sorted-lists/zhaoweifeng.c
@@ -1,40 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if(l1==NULL) return l2;
-        if(l2==NULL) return l1;
-
-    	ListNode *result,*tail;
-    
-    	if(l1->val < l2->val)
-    	{
-    	    result=l1;
-    		tail=l1;
-    		l1=l1->next;
-    	}
-    	else
-    	{
-    	    result=l2;
-    		tail=l2;
-    		l2=l2->next;
-    	}
-    	
-    	while(l1!=NULL && l2!=NULL)
-    	{
-    		if(l1->val < l2->val)
-    		{
-    			tail->next=l1;
-    			l1=l1->next;
-    		}
-    		else
-    		{
-    			tail->next=l2;
-    			l2=l2->next;
-    		}
-    		tail=tail->next;
-    	}
-    	if(l1==NULL) tail->next=l2;
-    	if(l2==NULL) tail->next=l1;
-    	return result;	
+        ListNode *result = new ListNode(0);
+        ListNode *cur=result;
+        
+        while(l1!=NULL && l2!=NULL)
+        {
+            cur->next = l1->val < l2->val ? l1 : l2 ;
+            l1->val < l2->val ? l1=l1->next : l2=l2->next ;
+            cur=cur->next;
+        }
+        
+        if(l1==NULL) cur->next = l2;
+        if(l2==NULL) cur->next = l1;
+        
+    	return result->next;	
     }
 };
