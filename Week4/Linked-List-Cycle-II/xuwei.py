20f4b94dcb6697badf8c1d5cc2b62ca925147205
highya <xwiscas@163.com>
2015-01-09 00:45:50 +0800

    Update xuwei.py

--- Week4/Linked-List-Cycle-II/xuwei.py
+++ Week4/Linked-List-Cycle-II/xuwei.py
@@ -1,1 +1,11 @@
-x
+class Solution:
+    def detectCycle(self, head):
+        p1,p2 = head,head
+        while p2 and p2.next:
+            p1,p2 = p1.next,p2.next.next
+            if p1 == p2:
+                p1 = head
+                while p1 != p2:
+                    p1,p2 = p1.next,p2.next
+                return p1
+        return None
