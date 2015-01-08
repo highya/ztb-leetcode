ae620e3335967d234678bd85482a7e495d2c8494
highya <xwiscas@163.com>
2015-01-08 23:54:24 +0800

    Update xuwei.py

--- Week3/3Sum-Closet/xuwei.py
+++ Week3/3Sum-Closet/xuwei.py
@@ -1,1 +1,18 @@
-s
+class Solution:
+    def threeSumClosest(self, num, target):
+        num.sort()
+        res = float("inf")
+        for i in range(len(num)-2):
+            l,r = i+1,len(num)-1
+            while l < r:
+                sum = num[i] + num[l] + num[r]
+                dis = target-sum
+                if abs(dis) < abs(res):
+                    res = dis
+                if  dis == 0:
+                    return target
+                elif dis < 0:
+                    r -= 1
+                else:
+                    l += 1
+        return target-res
