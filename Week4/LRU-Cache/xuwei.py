01ae676ca1b2cc38fb57e2ffe45b9ba9f0373de3
highya <xwiscas@163.com>
2015-01-09 02:07:22 +0800

    Update xuwei.py

--- Week4/LRU-Cache/xuwei.py
+++ Week4/LRU-Cache/xuwei.py
@@ -1,1 +1,25 @@
-x
+class LRUCache:
+    def __init__(self, capacity):
+        self.lst=[]
+        self.dct={}
+        self.cap=capacity
+
+    def get(self, key):
+        if key in self.dct:
+            self.lst.remove([key,self.dct[key]])
+            self.lst.append([key,self.dct[key]])
+            return self.dct[key]
+        else:
+            return -1
+
+    def set(self, key, value):
+        if key in self.dct:
+            self.lst.remove([key,self.dct[key]])
+            self.dct[key] = value
+            self.lst.append([key,self.dct[key]])
+        else:
+            if len(self.lst) == self.cap:
+                del self.dct[self.lst[0][0]]
+                del self.lst[0]
+            self.dct[key] = value
+            self.lst.append([key,self.dct[key]])
