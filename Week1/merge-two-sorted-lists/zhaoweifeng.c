d93ef1c260ef08ce614117f84056e0272e7d2efa
wox88 <wox88@qq.com>
2015-01-08 13:02:12 +0800

    Update zhaoweifeng.c

--- Week1/merge-two-sorted-lists/zhaoweifeng.c
+++ Week1/merge-two-sorted-lists/zhaoweifeng.c
@@ -14,13 +14,13 @@ public:
         
         while(l1!=NULL && l2!=NULL)
         {
+            //选择较小值结点，并改变相应指针
             cur->next = l1->val < l2->val ? l1 : l2 ;
             l1->val < l2->val ? l1=l1->next : l2=l2->next ;
             cur=cur->next;
         }
         
-        if(l1==NULL) cur->next = l2;
-        if(l2==NULL) cur->next = l1;
+        cur->next = l1==NULL ? l2 : l1;
         
     	return result->next;	
     }
