d576bedb4c67cfb2c829dfa21ab532a89037051b
highya <xwiscas@163.com>
2015-01-09 18:26:53 +0800

    Create xuwei.py

---
+++ Week5/Unique-Binary-Search-Trees/xuwei.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numTrees(self, n):
+        if n < 2:
+            return n
+        dct = {}
+        dct[0],dct[1] = 1,1
+        for i in range(2,n+1):
+            dct[i] = 0
+            for l in range(0,i):
+                dct[i] += dct[l]*dct[i-l-1]
+        return dct[n]
