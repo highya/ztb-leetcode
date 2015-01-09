076a58590f5b7b0fa6ca86948d0ed58fcde9ef0c
highya <xwiscas@163.com>
2015-01-09 19:10:05 +0800

    Update xuwei.py

--- Week5/Maximum-Subarray/xuwei.py
+++ Week5/Maximum-Subarray/xuwei.py
@@ -2,7 +2,7 @@ class Solution:
     def maxSubArray(self, A):
         maxsum,tmp = float("-inf"),float("-inf")
         for item in A:
-            tmp = item if tmp < 0 else tmp+item 
+            tmp = tmp + item if tmp > 0 else item 
             if tmp > maxsum:
                 maxsum = tmp
         return maxsum
