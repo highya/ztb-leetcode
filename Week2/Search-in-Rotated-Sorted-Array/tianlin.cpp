0f34099edbb59b4ee329adf9dede122a8fd99f94
linlanfeng <linlanfeng@users.noreply.github.com>
2015-01-15 17:25:39 +0800

    Update tianlin.cpp

--- Week2/Search-in-Rotated-Sorted-Array/tianlin.cpp
+++ Week2/Search-in-Rotated-Sorted-Array/tianlin.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int search(int A[], int n, int target) {
-        int num = n;
-        int position_head = 0;
-        int position_tail = num-1;
+        int num = n,position_head = 0,position_tail = num-1;
         bool isHead = false;
         if(A[position_head] == target)
             return position_head;
@@ -11,28 +9,15 @@ public:
             isHead = true;
         position_head++;
         while(position_head <= position_tail){
-            if(position_head == position_tail){
-                if(target == A[position_head])
-                    return position_head;
+            if((position_head == position_tail) && target != A[position_head])
                 break;
-            }
             int pos = (position_head + position_tail)/2;
             if(A[pos] == target)
                 return pos;
-            else if(A[pos] < target && A[pos] >= A[0])
+            else if((A[pos] < target && A[pos] >= A[0]) || (A[pos] < target && A[pos] < A[0] && !isHead) || (A[pos] > target && A[pos] > A[0] && !isHead))
                 position_head = pos+1;
-            else if(A[pos] < target && A[pos] < A[0] && !isHead)
-                position_head = pos+1;
-            else if(A[pos] < target && A[pos] < A[0] && isHead)
+            else if((A[pos] < target && A[pos] < A[0] && isHead) || (A[pos] > target && A[pos] <= A[0]) || (A[pos] > target && A[pos] > A[0] && isHead))
                 position_tail = pos-1;
-            else if(A[pos] > target && A[pos] <= A[0])
-                position_tail = pos-1;
-            else if(A[pos] > target && A[pos] > A[0] && !isHead)
-                position_head = pos+1;
-            else if(A[pos] > target && A[pos] > A[0] && isHead)
-                position_tail = pos-1;
-            else
-                position_head = pos+1;
         }
         return -1;
     }
