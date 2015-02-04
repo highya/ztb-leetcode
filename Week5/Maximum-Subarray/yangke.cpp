f1f50c81d8d5b679cf68007a0833a041aa183bfe
yangke <123yangke321@sina.com>
2015-02-04 09:43:58 +0800

    Create yangke.cpp

---
+++ Week5/Maximum-Subarray/yangke.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(int A[], int n) {
+        int result=INT_MIN,f=0;
+        for(int i=0;i<n;i++)
+        {
+            f=max(f+A[i],A[i]);
+            result=max(f,result);
+        }
+        return result;
+    }
+};
