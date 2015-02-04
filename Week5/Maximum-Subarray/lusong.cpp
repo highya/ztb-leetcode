0380f3afed62b0d6e8a7a053321f13259bc87b65
rou1song <lu__song@yeah.net>
2015-02-04 11:17:57 +0800

    Update lusong.cpp

--- Week5/Maximum-Subarray/lusong.cpp
+++ Week5/Maximum-Subarray/lusong.cpp
@@ -9,21 +9,16 @@ the contiguous subarray [4,−1,2,1] has the largest sum = 6.
 class Solution {
 public:
     int maxSubArray(int A[], int n) {
-        //f(i): 以第i位结尾的最大子串,再探索A[i+1]，若f（i-1）<= 0 ,...若 f（i-1）> 0 ,....
+        //f(i): 以第i位结尾的最大子串
         int B[n];
         B[0] = A[0];
-        for(int i = 1; i < n; i++){
-            if(B[i-1] <= 0) 
-                B[i] = A[i];
-            else 
-                B[i] = B[i-1] + A[i];
-        }
+        for(int i = 1; i < n; i++)
+                B[i] = max(A[i] , B[i-1] + A[i]);
         //求得所有以A[i]结尾的最大子串后，遍历数组B[],求其最大值max
         int max = B[0];
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++)
             if (B[i] > max) 
                 max = B[i];
-        }
         return max;
     }
 };
