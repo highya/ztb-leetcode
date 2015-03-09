f2e123d476d5046fca1195ef149ebcefd36464e6
wox88 <wox88@qq.com>
2015-03-09 20:47:57 +0800

    Create zhaoweifeng.cpp

---
+++ Week6/Valid-Palindrome/zhaoweifeng.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0,right=s.length()-1;
+        
+        while(left<right)
+        {
+            if(!isalnum(s[left])) {
+                left++;
+                continue;
+            }
+            
+            if(!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+
+            if(tolower(s[left])==tolower(s[right])) {
+                left++;
+                right--;
+            }
+            else return false;
+        }
+        
+        return true;
+    }
+};
